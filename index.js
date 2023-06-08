d2b0f0bd36c8090eed7c1d0f47d2b508d9878553
myra-k <myrakhatoon90@gmail.com>
2023-06-08 15:09:02 +0100

    added questions

--- index.js
+++ index.js
@@ -1,7 +1,53 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+const generateMarkdown= require('./utils/generateMarkdown');
+
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'github',
+        message: 'What is your Github Username?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a description for your project',
+    },
+    {
+        type: 'list',
+        name: 'licence',
+        message: ['MIT', 'APACHE 2.0', 'GPL 3.0', 'BSD 3', 'None'],
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'How do you install your projecr?',
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'How do you test your project?',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What does the user need to know when using the repo?',
+    },
+    {
+        type: 'input',
+        name: 'contributions',
+        message: 'What does the user need to know about constributing to the repo?',
+    },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
